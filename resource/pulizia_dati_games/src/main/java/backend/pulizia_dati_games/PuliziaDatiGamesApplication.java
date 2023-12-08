dd7403a9e7efd5803b4a27983d41b143e8cdae13
Pacchiotti Marco <57013700+pacchio1@users.noreply.github.com>
2023-12-08 17:22:05 +0100

    yes sir spring per solo pulizia dati

---
+++ resource/pulizia_dati_games/src/main/java/backend/pulizia_dati_games/PuliziaDatiGamesApplication.java
@@ -0,0 +1,13 @@
+package backend.pulizia_dati_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuliziaDatiGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuliziaDatiGamesApplication.class, args);
+	}
+
+}
---
+++ resource/pulizia_dati_games/src/test/java/backend/pulizia_dati_games/PuliziaDatiGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.pulizia_dati_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuliziaDatiGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
