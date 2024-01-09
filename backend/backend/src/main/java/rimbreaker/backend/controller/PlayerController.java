498e687e8f5023ccf9867645fbb41ceb4337fbd2
TempoTea <giorgio.magliocca@edu.itspiemonte.it>
2024-01-09 16:23:02 +0100

    Revert "updated Player + to-do list"
    This reverts commit a48e64ea3a289f3c01c230007019f55af7885849.

--- backend/backend/src/main/java/rimbreaker/backend/controller/PlayerController.java
+++ backend/backend/src/main/java/rimbreaker/backend/controller/PlayerController.java
@@ -1,6 +1,7 @@
 package rimbreaker.backend.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,29 +12,19 @@ import rimbreaker.backend.service.PlayerService;
 @RequiredArgsConstructor
 @RequestMapping("/player")
 public class PlayerController {
-
         private final PlayerService playerService;
 
         // Returns player details by ID
-        @GetMapping("/number")
-        public ResponseEntity<?> getPlayerById(Long id) {
-
-            return playerService.getPlayerById(id);
-
+        @GetMapping("/{id}")
+        public ResponseEntity<?> getPlayer(@PathVariable Long id) {
+            return playerService.getPlayer(id);
         }
 
         // Returns all players
         @GetMapping("/all")
         public ResponseEntity<?> getAllPlayers() {
-
             return playerService.getAllPlayers();
-
         }
+    }
 
-        @GetMapping("/team")
-        public ResponseEntity<?> getPlayersByTeam(Long id_team) {
-
-            return playerService.getPlayersByTeam(id_team);
-
-        }
 }
--- backend/backend/src/main/java/rimbreaker/backend/repository/PlayerRepository.java
+++ backend/backend/src/main/java/rimbreaker/backend/repository/PlayerRepository.java
@@ -2,27 +2,14 @@ package rimbreaker.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import rimbreaker.backend.entity.Player;
-import rimbreaker.backend.payload.response.ResponsePlayer;
 
 import java.util.List;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
     @Query("SELECT p FROM Player p WHERE p.idPlayer = :id")
-    Player getPlayerById(@Param("id") Long id);
-
-    @Query("SELECT new rimbreaker.backend.payload.response.ResponsePlayer(" +
-            "t.id, " +
-            "p.idPlayer, " +
-            "p.season, " +
-            "p.name, " +
-            "p.surname" +
-            ") FROM Player p " +
-            "JOIN Team t ON t.id = p.idTeam " +
-            "WHERE p.idTeam = :id_team")
-    List<ResponsePlayer> getPlayersByTeam(@Param("id_team") Long id_team);
+    Player getPlayerById(Long id);
 
     @Query("SELECT p FROM Player p")
     List<Player> getAllPlayers();
