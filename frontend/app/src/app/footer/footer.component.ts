0b6b8024bbe51919379fe663f9b24167c4431fb5
Carloechitta <127096919+Carloechitta@users.noreply.github.com>
2024-01-24 22:38:44 +0100

    ulteriori aggiustamenti

--- frontend/app/src/app/footer/footer.component.ts
+++ frontend/app/src/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ThemeService } from '../_service/dark-mode.service';
+import { LocalStorageService } from '../_service/localStorage.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,12 +8,29 @@ import { ThemeService } from '../_service/dark-mode.service';
 })
 export class FooterComponent {
 
-  constructor(public themeService: ThemeService) {}
+  loggedIn: boolean = false;
 
-  ngOnInit(): void {}
+  constructor(public themeService: ThemeService, private localStorageService: LocalStorageService) {}
+
+  ngOnInit(): void {
+    this.themeService.themeSettled();
+
+    if (localStorage.getItem('emailAccount')) {
+      this.setLoggedInState();
+    }
+
+    // Sottoscrivi all'observable per i cambiamenti nel localStorage
+    this.localStorageService.getStorageChangeObservable().subscribe(() => {
+      this.setLoggedInState();
+    }); 
+  }
 
   toggleTheme(): void {
     this.themeService.toggleTheme();
   }
-  
+
+  setLoggedInState(): void {
+    this.loggedIn = localStorage.getItem('emailAccount') !== null;
+    console.log(this.loggedIn);
+  }
 }
